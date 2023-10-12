c7d15a763ea57149ce18803f2d62e6046a3ce0e6
mwasilew <01158977@pw.edu.pl>
2023-10-12 18:55:39 +0200

    Add scripts to install mysql db

---
+++ src/main/db/mysql/init_db.sql
@@ -0,0 +1,16 @@
+CREATE USER IF NOT EXISTS '%db_user%'@'%' IDENTIFIED BY '%db_password%';
+
+CREATE DATABASE IF NOT EXISTS cache;
+
+ALTER DATABASE cache
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON cache.* TO '%db_user%'@'%' WITH GRANT OPTION;
+
+USE cache;
+
+CREATE TABLE IF NOT EXISTS factorization_results (
+	number		INT PRIMARY KEY
+	, factors	JSON NOT NULL
+);
