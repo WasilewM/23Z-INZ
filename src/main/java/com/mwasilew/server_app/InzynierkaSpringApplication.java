bacad3cf04e6b24fb71b9da09591465b80de65da
mwasilew <01158977@pw.edu.pl>
2023-10-17 18:29:26 +0200

    Create project from IntelliJ Spring Initializr

---
+++ src/main/java/com/mwasilew/server_app/InzynierkaSpringApplication.java
@@ -0,0 +1,14 @@
+package com.mwasilew.server_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzynierkaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InzynierkaSpringApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/test/java/com/mwasilew/server_app/InzynierkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwasilew.server_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InzynierkaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
