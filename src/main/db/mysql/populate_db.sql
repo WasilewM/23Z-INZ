72bc6e99beaff827621bd775779506b656970323
mwasilew <01158977@pw.edu.pl>
2023-10-19 13:56:05 +0200

    Update populate_db.sql

--- src/main/db/mysql/populate_db.sql
+++ src/main/db/mysql/populate_db.sql
@@ -1,20 +1,20 @@
 USE cache;
 
 INSERT into factorization_results
-VALUES (1, '{"factors": [1]}');
+VALUES (1, '[1]');
 INSERT into factorization_results
-VALUES (2, '{"factors": [1, 2]}');
+VALUES (2, '[1, 2]');
 INSERT into factorization_results
-VALUES (4, '{"factors": [1, 2, 2]}');
+VALUES (4, '[1, 2, 2]');
 INSERT into factorization_results
-VALUES (8, '{"factors": [1, 2, 2, 2]}');
+VALUES (8, '[1, 2, 2, 2]');
 INSERT into factorization_results
-VALUES (16, '{"factors": [1, 2, 2, 2, 2]}');
+VALUES (16, '[1, 2, 2, 2, 2]');
 INSERT into factorization_results
-VALUES (32, '{"factors": [1, 2, 2, 2, 2, 2]}');
+VALUES (32, '[1, 2, 2, 2, 2, 2]');
 INSERT into factorization_results
-VALUES (64, '{"factors": [1, 2, 2, 2, 2, 2]}');
+VALUES (64, '[1, 2, 2, 2, 2, 2]');
 INSERT into factorization_results
-VALUES (128, '{"factors": [1, 2, 2, 2, 2, 2, 2]}');
+VALUES (128, '[1, 2, 2, 2, 2, 2, 2]');
 
 COMMIT;
