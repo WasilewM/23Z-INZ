92b764129935da349a63fa1d640533f2f046609f
mwasilew <01158977@pw.edu.pl>
2023-10-25 21:12:32 +0200

    Move responsibility of query results checking from controller to service

--- src/main/java/com/mwasilew/server_app/controllers/FactorizationResultController.java
+++ src/main/java/com/mwasilew/server_app/controllers/FactorizationResultController.java
@@ -1,6 +1,5 @@
 package com.mwasilew.server_app.controllers;
 
-import com.mwasilew.server_app.NumberFactorizor;
 import com.mwasilew.server_app.models.FactorizationResult;
 import com.mwasilew.server_app.services.FactorizationResultService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
@@ -25,40 +23,6 @@ public class FactorizationResultController {
 
     @GetMapping("{number}")
     public Optional<FactorizationResult> getFactorizationResult(@PathVariable int number) {
-        long calcStartTime = System.currentTimeMillis();
-        Optional<FactorizationResult> cacheResult = factorizationResultService.getFactorizationResultFor(number);
-        if (cacheResult.isEmpty()) {
-            return handleRequestCalculations(number, calcStartTime);
-        }
-        return handleRequestsWithoutCalculations(number, calcStartTime, cacheResult);
-    }
-
-    private Optional<FactorizationResult> handleRequestCalculations(int number, long calcStartTime) {
-        synchronized (FactorizationResultController.class) {
-            Optional<FactorizationResult> cacheResult = factorizationResultService.getFactorizationResultFor(number);
-            if (cacheResult.isEmpty()) {
-                FactorizationResult factorizationResult = calculateFactorizationResult(number);
-                factorizationResultService.saveFactorizationResult(factorizationResult);
-                long calcEndTime = System.currentTimeMillis();
-                long calcTime = calcEndTime - calcStartTime;
-                System.out.println("Not found in cache: " + number + " - took: " + calcTime);
-                return Optional.of(factorizationResult);
-            } else {
-                return handleRequestsWithoutCalculations(number, calcStartTime, cacheResult);
-            }
-        }
-    }
-
-    private static FactorizationResult calculateFactorizationResult(int number) {
-        NumberFactorizor nr = new NumberFactorizor();
-        ArrayList<Integer> factors = nr.factorize(number);
-        return new FactorizationResult(number, factors.toString());
-    }
-
-    private static Optional<FactorizationResult> handleRequestsWithoutCalculations(int number, long calcStartTime, Optional<FactorizationResult> cacheResult) {
-        long calcEndTime = System.currentTimeMillis();
-        long calcTime = calcEndTime - calcStartTime;
-        System.out.println("Cache hit for: " + number + " - took: " + calcTime);
-        return cacheResult;
+        return factorizationResultService.getFactorizationResultFor(number);
     }
 }
--- src/main/java/com/mwasilew/server_app/services/FactorizationResultService.java
+++ src/main/java/com/mwasilew/server_app/services/FactorizationResultService.java
@@ -1,13 +1,18 @@
 package com.mwasilew.server_app.services;
 
+import com.mwasilew.server_app.NumberFactorizor;
+import com.mwasilew.server_app.controllers.FactorizationResultController;
 import com.mwasilew.server_app.models.FactorizationResult;
 import com.mwasilew.server_app.repositories.FactorizationResultRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional
 public class FactorizationResultService {
@@ -19,10 +24,44 @@ public class FactorizationResultService {
     }
 
     public Optional<FactorizationResult> getFactorizationResultFor(int number) {
-        return factorizationResultRepository.findById(number);
+        long calcStartTime = System.currentTimeMillis();
+        Optional<FactorizationResult> cacheResult = factorizationResultRepository.findById(number);
+        if (cacheResult.isEmpty()) {
+            return handleRequestCalculations(number, calcStartTime);
+        }
+        return handleRequestsWithoutCalculations(number, calcStartTime, cacheResult);
     }
 
     public void saveFactorizationResult(FactorizationResult factorizationResult) {
         factorizationResultRepository.save(factorizationResult);
+    }
+
+    private Optional<FactorizationResult> handleRequestCalculations(int number, long calcStartTime) {
+        synchronized (FactorizationResultController.class) {
+            Optional<FactorizationResult> cacheResult = factorizationResultRepository.findById(number);
+            if (cacheResult.isEmpty()) {
+                FactorizationResult factorizationResult = calculateFactorizationResult(number);
+                this.saveFactorizationResult(factorizationResult);
+                long calcEndTime = System.currentTimeMillis();
+                long calcTime = calcEndTime - calcStartTime;
+                log.info("Not found in cache: " + number + " - took: " + calcTime);
+                return Optional.of(factorizationResult);
+            } else {
+                return handleRequestsWithoutCalculations(number, calcStartTime, cacheResult);
+            }
+        }
+    }
+
+    private static FactorizationResult calculateFactorizationResult(int number) {
+        NumberFactorizor nr = new NumberFactorizor();
+        ArrayList<Integer> factors = nr.factorize(number);
+        return new FactorizationResult(number, factors.toString());
+    }
+
+    private static Optional<FactorizationResult> handleRequestsWithoutCalculations(int number, long calcStartTime, Optional<FactorizationResult> cacheResult) {
+        long calcEndTime = System.currentTimeMillis();
+        long calcTime = calcEndTime - calcStartTime;
+        log.info("Cache hit for: " + number + " - time taken: " + calcTime);
+        return cacheResult;
     }
 }
