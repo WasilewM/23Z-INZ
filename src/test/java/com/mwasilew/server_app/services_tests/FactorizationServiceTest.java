65270306a5c17929e57942f3f0623c4909ec2b8f
mwasilew <01158977@pw.edu.pl>
2023-10-29 18:47:13 +0100

    Add tests for FactorizationService

---
+++ src/test/java/com/mwasilew/server_app/services_tests/FactorizationServiceTest.java
@@ -0,0 +1,61 @@
+package com.mwasilew.server_app.services_tests;
+
+import com.mwasilew.server_app.models.FactorizationResult;
+import com.mwasilew.server_app.repositories.FactorizationRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+public class FactorizationServiceTest {
+    @TestConfiguration
+    public static class FactorizationServiceTestContextConfiguration {
+        @Bean
+        public FactorizationServiceWithMethodsCallingCounters factorizationService(FactorizationRepository fr) {
+            return new FactorizationServiceWithMethodsCallingCounters(fr);
+        }
+    }
+
+    @Autowired
+    private FactorizationServiceWithMethodsCallingCounters factorizationService;
+    @MockBean
+    private FactorizationRepository factorizationRepository;
+
+    @Before
+    public void setUp() {
+        factorizationService.resetMethodCallsCounters();
+    }
+
+    @Test
+    public void givenFactorizationServiceWithMockedValueInDB_whenAskedForMockedValue_thenCalculationsAreNotPerformed() {
+        int request = 5;
+        Optional<FactorizationResult> factorizationResults = Optional.of(new FactorizationResult(5, "[1, 5]"));
+        Mockito.when(factorizationRepository.findById(request)).thenReturn(factorizationResults);
+        factorizationService.getFactorizationResultFor(request);
+
+        assertEquals(0, factorizationService.handleRequestCalculationsMethodCalls);
+        assertEquals(0, factorizationService.calculateFactorizationResultMethodCalls);
+        assertEquals(0, factorizationService.saveFactorizationResultMethodCalls);
+    }
+
+    @Test
+    public void givenFactorizationServiceWithoutValuesInDB_whenAskedForValue_thenCalculationsArePerformed() {
+        int request = 5;
+        Mockito.when(factorizationRepository.findById(request)).thenReturn(Optional.empty());
+        factorizationService.getFactorizationResultFor(request);
+
+        assertEquals(1, factorizationService.handleRequestCalculationsMethodCalls);
+        assertEquals(1, factorizationService.calculateFactorizationResultMethodCalls);
+        assertEquals(1, factorizationService.saveFactorizationResultMethodCalls);
+    }
+}
---
+++ src/test/java/com/mwasilew/server_app/services_tests/FactorizationServiceWithMethodsCallingCounters.java
@@ -0,0 +1,46 @@
+package com.mwasilew.server_app.services_tests;
+
+import com.mwasilew.server_app.models.FactorizationResult;
+import com.mwasilew.server_app.repositories.FactorizationRepository;
+import com.mwasilew.server_app.services.FactorizationService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FactorizationServiceWithMethodsCallingCounters extends FactorizationService {
+    public int handleRequestCalculationsMethodCalls;
+    public int calculateFactorizationResultMethodCalls;
+    public int saveFactorizationResultMethodCalls;
+
+    public FactorizationServiceWithMethodsCallingCounters(FactorizationRepository factorizationRepository) {
+        super(factorizationRepository);
+        handleRequestCalculationsMethodCalls = 0;
+        calculateFactorizationResultMethodCalls = 0;
+        saveFactorizationResultMethodCalls = 0;
+    }
+
+    public void resetMethodCallsCounters() {
+        handleRequestCalculationsMethodCalls = 0;
+        calculateFactorizationResultMethodCalls = 0;
+        saveFactorizationResultMethodCalls = 0;
+    }
+
+    @Override
+    protected Optional<FactorizationResult> handleRequestCalculations(int number) {
+        handleRequestCalculationsMethodCalls += 1;
+        return super.handleRequestCalculations(number);
+    }
+
+    @Override
+    protected FactorizationResult calculateFactorizationResult(int number) {
+        calculateFactorizationResultMethodCalls += 1;
+        return super.calculateFactorizationResult(number);
+    }
+
+    @Override
+    protected void saveFactorizationResult(FactorizationResult factorizationResult) {
+        saveFactorizationResultMethodCalls += 1;
+        super.saveFactorizationResult(factorizationResult);
+    }
+}
