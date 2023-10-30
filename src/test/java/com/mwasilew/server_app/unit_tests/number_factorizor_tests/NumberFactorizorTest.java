68b1ec97901199167df6c0f925656f60e44c4a92
mwasilew <01158977@pw.edu.pl>
2023-10-30 20:29:49 +0100

    Remove org.testng testing dependency to remove vulnerabilities CVE-2022-1471 and CVE-2022-41854

--- src/test/java/com/mwasilew/server_app/unit_tests/number_factorizor_tests/NumberFactorizorTest.java
+++ src/test/java/com/mwasilew/server_app/unit_tests/number_factorizor_tests/NumberFactorizorTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.testng.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class NumberFactorizorTest {
     @Test
