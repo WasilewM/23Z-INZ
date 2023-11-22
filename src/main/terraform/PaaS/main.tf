c33029c976c6f3c6f03c9d2eec7269c212cede88
mwasilew <01158977@pw.edu.pl>
2023-11-22 13:42:44 +0100

    Simplify resources naming convention in PaaS/main.tf

--- src/main/terraform/PaaS/main.tf
+++ src/main/terraform/PaaS/main.tf
@@ -11,8 +11,8 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "test-01-paas-rg" {
-  name     = "test-01-paas-rg"
+resource "azurerm_resource_group" "paas-rg" {
+  name     = "paas-rg"
   location = "polandcentral"
 
   tags = {
@@ -20,10 +20,10 @@ resource "azurerm_resource_group" "test-01-paas-rg" {
   }
 }
 
-resource "azurerm_virtual_network" "test-01-paas-vnet" {
-  name                = "test-01-paas-network"
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
-  location            = azurerm_resource_group.test-01-paas-rg.location
+resource "azurerm_virtual_network" "paas-vnet" {
+  name                = "paas-network"
+  resource_group_name = azurerm_resource_group.paas-rg.name
+  location            = azurerm_resource_group.paas-rg.location
   address_space       = ["10.0.1.0/24"]
 
   tags = {
@@ -31,25 +31,25 @@ resource "azurerm_virtual_network" "test-01-paas-vnet" {
   }
 }
 
-resource "azurerm_subnet" "test-01-paas-subnet-01" {
-  name                 = "test-01-paas-subnet-01"
-  resource_group_name  = azurerm_resource_group.test-01-paas-rg.name
-  virtual_network_name = azurerm_virtual_network.test-01-paas-vnet.name
+resource "azurerm_subnet" "paas-subnet-01" {
+  name                 = "paas-subnet-01"
+  resource_group_name  = azurerm_resource_group.paas-rg.name
+  virtual_network_name = azurerm_virtual_network.paas-vnet.name
   address_prefixes     = ["10.0.1.0/26"]
 }
 
-resource "azurerm_network_security_group" "test-01-paas-sg-01" {
-  name                = "test-01-paas-sg-01"
-  location            = azurerm_resource_group.test-01-paas-rg.location
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+resource "azurerm_network_security_group" "paas-sg-01" {
+  name                = "paas-sg-01"
+  location            = azurerm_resource_group.paas-rg.location
+  resource_group_name = azurerm_resource_group.paas-rg.name
 
   tags = {
     environment = "PaaS"
   }
 }
 
-resource "azurerm_network_security_rule" "test-01-paas-rule-dev-01" {
-  name                        = "test-01-paas-rule-dev-01"
+resource "azurerm_network_security_rule" "paas-rule-dev-01" {
+  name                        = "paas-rule-dev-01"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
@@ -58,37 +58,37 @@ resource "azurerm_network_security_rule" "test-01-paas-rule-dev-01" {
   destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.test-01-paas-rg.name
-  network_security_group_name = azurerm_network_security_group.test-01-paas-sg-01.name
+  resource_group_name         = azurerm_resource_group.paas-rg.name
+  network_security_group_name = azurerm_network_security_group.paas-sg-01.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "test-01-paas-rule-dev-01-sga" {
-  subnet_id                 = azurerm_subnet.test-01-paas-subnet-01.id
-  network_security_group_id = azurerm_network_security_group.test-01-paas-sg-01.id
+resource "azurerm_subnet_network_security_group_association" "paas-rule-dev-01-sga" {
+  subnet_id                 = azurerm_subnet.paas-subnet-01.id
+  network_security_group_id = azurerm_network_security_group.paas-sg-01.id
 }
 
 # VM for observability
-resource "azurerm_public_ip" "test-01-paas-public-ip-observability" {
-  name                = "test-01-paas-public-ip-observability"
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
-  location            = azurerm_resource_group.test-01-paas-rg.location
+resource "azurerm_public_ip" "paas-public-ip-observability" {
+  name                = "paas-public-ip-observability"
+  resource_group_name = azurerm_resource_group.paas-rg.name
+  location            = azurerm_resource_group.paas-rg.location
   allocation_method   = "Dynamic"
   tags = {
     environment = "PaaS"
   }
 }
 
-resource "azurerm_network_interface" "test-01-paas-nic-observability" {
-  name                = "test-01-paas-nic-observability"
-  location            = azurerm_resource_group.test-01-paas-rg.location
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+resource "azurerm_network_interface" "paas-nic-observability" {
+  name                = "paas-nic-observability"
+  location            = azurerm_resource_group.paas-rg.location
+  resource_group_name = azurerm_resource_group.paas-rg.name
 
   ip_configuration {
-    name                          = "test-01-paas-internal-observability"
-    subnet_id                     = azurerm_subnet.test-01-paas-subnet-01.id
+    name                          = "paas-internal-observability"
+    subnet_id                     = azurerm_subnet.paas-subnet-01.id
     private_ip_address_allocation = "Static"
     private_ip_address            = var.observability_private_ip
-    public_ip_address_id          = azurerm_public_ip.test-01-paas-public-ip-observability.id
+    public_ip_address_id          = azurerm_public_ip.paas-public-ip-observability.id
   }
 
   tags = {
@@ -96,13 +96,13 @@ resource "azurerm_network_interface" "test-01-paas-nic-observability" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "test-01-paas-vm-observability" {
-  name                  = "test-01-paas-vm-observability"
-  resource_group_name   = azurerm_resource_group.test-01-paas-rg.name
-  location              = azurerm_resource_group.test-01-paas-rg.location
+resource "azurerm_linux_virtual_machine" "paas-vm-observability" {
+  name                  = "paas-vm-observability"
+  resource_group_name   = azurerm_resource_group.paas-rg.name
+  location              = azurerm_resource_group.paas-rg.location
   size                  = "Standard_B1s"
   admin_username        = var.admin_username
-  network_interface_ids = [azurerm_network_interface.test-01-paas-nic-observability.id]
+  network_interface_ids = [azurerm_network_interface.paas-nic-observability.id]
 
   custom_data = filebase64("customdata_observability.tpl")
 
@@ -128,10 +128,10 @@ resource "azurerm_linux_virtual_machine" "test-01-paas-vm-observability" {
   }
 }
 
-resource "azurerm_service_plan" "test-01-paas-service-plan" {
-  name                = "test-01-paas-service-plan"
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
-  location            = azurerm_resource_group.test-01-paas-rg.location
+resource "azurerm_service_plan" "paas-service-plan" {
+  name                = "paas-service-plan"
+  resource_group_name = azurerm_resource_group.paas-rg.name
+  location            = azurerm_resource_group.paas-rg.location
   os_type             = "Linux"
   sku_name            = "B1"
 
@@ -140,11 +140,11 @@ resource "azurerm_service_plan" "test-01-paas-service-plan" {
   }
 }
 
-resource "azurerm_linux_web_app" "test-01-paas-server-app" {
-  name                = "test-01-paas-server-app"
-  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
-  location            = azurerm_service_plan.test-01-paas-service-plan.location
-  service_plan_id     = azurerm_service_plan.test-01-paas-service-plan.id
+resource "azurerm_linux_web_app" "paas-server-app" {
+  name                = "paas-server-app"
+  resource_group_name = azurerm_resource_group.paas-rg.name
+  location            = azurerm_service_plan.paas-service-plan.location
+  service_plan_id     = azurerm_service_plan.paas-service-plan.id
 
   site_config {
     application_stack {
