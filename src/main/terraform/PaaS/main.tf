50bd3bea80f08e95cc332714b4ff6e7a10901d01
mwasilew <01158977@pw.edu.pl>
2023-11-22 13:32:42 +0100

    Add observability VM to PaaS/main.tf

--- src/main/terraform/PaaS/main.tf
+++ src/main/terraform/PaaS/main.tf
@@ -14,8 +14,117 @@ provider "azurerm" {
 resource "azurerm_resource_group" "test-01-paas-rg" {
   name     = "test-01-paas-rg"
   location = "polandcentral"
+
   tags = {
-    environment = "test-paas"
+    environment = "PaaS"
+  }
+}
+
+resource "azurerm_virtual_network" "test-01-paas-vnet" {
+  name                = "test-01-paas-network"
+  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+  location            = azurerm_resource_group.test-01-paas-rg.location
+  address_space       = ["10.0.1.0/24"]
+
+  tags = {
+    environment = "PaaS"
+  }
+}
+
+resource "azurerm_subnet" "test-01-paas-subnet-01" {
+  name                 = "test-01-paas-subnet-01"
+  resource_group_name  = azurerm_resource_group.test-01-paas-rg.name
+  virtual_network_name = azurerm_virtual_network.test-01-paas-vnet.name
+  address_prefixes     = ["10.0.1.0/26"]
+}
+
+resource "azurerm_network_security_group" "test-01-paas-sg-01" {
+  name                = "test-01-paas-sg-01"
+  location            = azurerm_resource_group.test-01-paas-rg.location
+  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+
+  tags = {
+    environment = "PaaS"
+  }
+}
+
+resource "azurerm_network_security_rule" "test-01-paas-rule-dev-01" {
+  name                        = "test-01-paas-rule-dev-01"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.test-01-paas-rg.name
+  network_security_group_name = azurerm_network_security_group.test-01-paas-sg-01.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "test-01-paas-rule-dev-01-sga" {
+  subnet_id                 = azurerm_subnet.test-01-paas-subnet-01.id
+  network_security_group_id = azurerm_network_security_group.test-01-paas-sg-01.id
+}
+
+# VM for observability
+resource "azurerm_public_ip" "test-01-paas-public-ip-observability" {
+  name                = "test-01-paas-public-ip-observability"
+  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+  location            = azurerm_resource_group.test-01-paas-rg.location
+  allocation_method   = "Dynamic"
+  tags = {
+    environment = "PaaS"
+  }
+}
+
+resource "azurerm_network_interface" "test-01-paas-nic-observability" {
+  name                = "test-01-paas-nic-observability"
+  location            = azurerm_resource_group.test-01-paas-rg.location
+  resource_group_name = azurerm_resource_group.test-01-paas-rg.name
+
+  ip_configuration {
+    name                          = "test-01-paas-internal-observability"
+    subnet_id                     = azurerm_subnet.test-01-paas-subnet-01.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.observability_private_ip
+    public_ip_address_id          = azurerm_public_ip.test-01-paas-public-ip-observability.id
+  }
+
+  tags = {
+    environment = "PaaS"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "test-01-paas-vm-observability" {
+  name                  = "test-01-paas-vm-observability"
+  resource_group_name   = azurerm_resource_group.test-01-paas-rg.name
+  location              = azurerm_resource_group.test-01-paas-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.test-01-paas-nic-observability.id]
+
+  custom_data = filebase64("customdata_observability.tpl")
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "PaaS"
   }
 }
 
@@ -25,6 +134,10 @@ resource "azurerm_service_plan" "test-01-paas-service-plan" {
   location            = azurerm_resource_group.test-01-paas-rg.location
   os_type             = "Linux"
   sku_name            = "B1"
+
+  tags = {
+    environment = "PaaS"
+  }
 }
 
 resource "azurerm_linux_web_app" "test-01-paas-server-app" {
@@ -39,5 +152,9 @@ resource "azurerm_linux_web_app" "test-01-paas-server-app" {
       java_server_version = 17
       java_version        = 17
     }
+  }
+
+  tags = {
+    environment = "PaaS"
   }
 }
---
+++ src/main/terraform/PaaS/variables.tf
@@ -0,0 +1,11 @@
+variable "admin_username" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "observability_private_ip" {
+  type = string
+}
