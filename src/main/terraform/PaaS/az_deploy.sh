cfb5bbdb5cb33ae4bce74ca948dad26a4aa5a051
mwasilew <01158977@pw.edu.pl>
2023-11-28 17:28:18 +0100

    Add echo messages to az_deploy.sh

--- src/main/terraform/PaaS/az_deploy.sh
+++ src/main/terraform/PaaS/az_deploy.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
+
+echo "Creating cloud resources..."
+
+echo "-----------------------------------------------------"
+echo "Creating resource group"
 az group create --name paas-spring-rg --location northeurope
 
+echo "-----------------------------------------------------"
+echo "Creating Spring Apps Service instance"
 az spring create --name paas-spring-apps-svc --resource-group paas-spring-rg
 
+echo "-----------------------------------------------------"
+echo "Creating MySQL Flexible Server"
 az mysql flexible-server create \
   --resource-group paas-spring-rg \
   --name paas-spring-mysql-db \
@@ -15,6 +25,8 @@ az mysql flexible-server create \
   # Detected current client IP : A.B.C.D
   # Do you want to enable access to client A.B.C.D (y/n):
 
+echo "-----------------------------------------------------"
+echo "Creating Spring App instance"
 az spring app create \
 	--name paas-spring-server-app \
 	--service paas-spring-apps-svc \
@@ -22,6 +34,8 @@ az spring app create \
 	--assign-endpoint true \
 	--runtime-version Java_17
 
+echo "-----------------------------------------------------"
+echo "Creating connection between Spring App and MySQL DB"
 az spring connection create mysql-flexible \
   --resource-group paas-spring-rg \
   --service paas-spring-apps-svc \
@@ -32,6 +46,8 @@ az spring connection create mysql-flexible \
   --secret name=worker secret=wo^Ker_123 \
 	--client-type springBoot
 
+echo "-----------------------------------------------------"
+echo "Deploying Spring App"
 az spring app deploy \
   --name paas-spring-server-app \
   --service paas-spring-apps-svc \
@@ -39,4 +55,9 @@ az spring app deploy \
   --artifact-path ../../../../target/server_app-0.1.0.jar \
 	--runtime-version Java_17
 
-az spring app list  --resource-group paas-spring-rg --service paas-spring-apps-svc --output table
+echo "-----------------------------------------------------"
+echo "Cloud resources creations has finished"
+az spring app list \
+  --resource-group paas-spring-rg \
+  --service paas-spring-apps-svc \
+  --output table
